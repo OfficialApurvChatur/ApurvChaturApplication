272dfacc01bc8d38c2bbbab6ff866e1c76b5f313
ApurvChatur <chaturapurv@gmail.com>
2025-02-19 00:25:47 +0530

    fix: backend url updated

--- site/frontend/src/love/aAPI/APIBase.js
+++ site/frontend/src/love/aAPI/APIBase.js
@@ -7,9 +7,9 @@ const APIBase = (props) => {
         method: props.method,
         url: props.url,
         baseURL: import.meta.env.VITE_ENVIRONMENT === "Production" ? (
-            import.meta.env.VITE_APPLICATION === "ApurvChatur" ? "https://soup-apurv-chatur-backend.onrender.com" :
-            import.meta.env.VITE_APPLICATION === "AnushreeMandape" ? "https://soup-anushree-mandape-backend.onrender.com" :
-            import.meta.env.VITE_APPLICATION === "SofieBerkin" ? "https://soup-sofie-berkin-backend.onrender.com" : "NULL"
+            import.meta.env.VITE_APPLICATION === "ApurvChatur" ? "https://backend-for-apurv-chatur-application.onrender.com" :
+            import.meta.env.VITE_APPLICATION === "AnushreeMandape" ? "https://backend-for-anushree-mandape-application.onrender.com" :
+            import.meta.env.VITE_APPLICATION === "SofieBerkin" ? "https://backend-for-sofie-berkin-application.onrender.com" : "NULL"
         ) : "http://localhost:8000",
         headers: { 
             'Content-Type': 'application/json',
